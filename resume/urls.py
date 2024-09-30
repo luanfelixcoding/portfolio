df5e1edc4da9a0074327039cc62eb864b7363389
luanfelixcoding <luanfpcoding@gmail.com>
2024-09-30 01:15:02 -0300

    Adding other pages such as about, projects, experience and certificates HTML files. Adding new images just to test and customizing the new pages in style CSS file

--- resume/urls.py
+++ resume/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("projects/", views.projects, name="projects"),
+    path("experience/", views.experience, name="experience"),
+    path("certificate/", views.certificate, name="certificate")
 ]
--- resume/views.py
+++ resume/views.py
@@ -5,3 +5,35 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def projects(request):
+    projects_show = [
+        {"title": "Example",
+         "path": "images/python-logo.png"},
+        {"title": "Example2",
+         "path": "images/css-logo.png"},
+        {"title": "Example3",
+         "path": "images/html-logo.png"},
+    ]
+    return render(request, "projects.html", {"projects_show": projects_show})
+
+
+def experience(request):
+    experiences = [
+        {"company": "ABC",
+         "position": "python developer"},
+        {"company": "ABC2",
+         "position": "python developer2"},
+        {"company": "ABC3",
+         "position": "python developer3"}
+    ]
+    return render(request, "experience.html", {"experiences": experiences})
+
+
+def certificate(request):
+    return render(request, "certificate.html")
